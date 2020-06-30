52d2cf7dcde5fc84fa91d2a80eefce11f7f99f84
vitor-risso <vitor.risso13@gmail.com>
2020-06-30 13:24:41 -0300

    .

---
+++ aula_11_condicao/main.js
@@ -0,0 +1,9 @@
+ var vel = 60
+console.log (`A sua velocidade é de ${vel} km/h`)
+if (vel > 60) {
+    console.log(`Você foi multado`)
+
+    
+}
+console.log(`Dirija sempre com cinto de segurnça`) 
+
---
+++ aula_11_condicao/main1.js
@@ -0,0 +1,7 @@
+var pais = `França`
+console.log(`Você é do(a) ${pais}`)
+if (pais == `Brasil`){
+    console.log(`Brasileiro`)
+} else{
+console.log(`Estrangeiro`)
+}

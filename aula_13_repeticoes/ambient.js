57e0cb1a5567fbccdc0e4fcc9aa30c6dd0418b35
vitor-risso <vitor.risso13@gmail.com>
2020-07-07 23:43:54 -0300

    .

---
+++ aula_13_repeticoes/ambient.js
@@ -0,0 +1,19 @@
+/*console.log('ola')
+console.log('ola')
+console.log('ola')
+console.log('ola')
+console.log('ola')
+console.log('ola')
+console.log('ola')*/
+/*
+var i = 1
+
+while (i<999){
+    console.log(`${i}`)
+    i++
+} */ 
+ var i = 1
+do{
+    console.log('ola')
+    i ++
+} while (i<50)
